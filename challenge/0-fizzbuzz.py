bb3a45fc58c0b407d5dfcfaa7bebab4ee37a2cfc
TonNom <ton-email@example.com>
2025-05-12 15:57:21 +0200

    add fizz buzz

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,38 +1,38 @@
 #!/usr/bin/python3
-
+""" FizzBuzz
+"""
 import sys
-
 def fizzbuzz(n):
-
-
+    """
+    FizzBuzz function prints numbers from 1 to n separated by a space.
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
+    """
+    if n < 1:
+        return
+    tmp_result = []
     for i in range(1, n + 1):
-        # Vérifie d'abord si le nombre est divisible par 3 et 5 (priorité)
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
-        # Vérifie si le nombre est divisible par 3 uniquement
-        elif i % 3 == 0:
-            print("Fizz", end=" ")
-        # Vérifie si le nombre est divisible par 5 uniquement
-        elif i % 5 == 0:
-            print("Buzz", end=" ")
-        # Si le nombre n'est divisible ni par 3 ni par 5, on l'affiche directement
+        if (i % 3 == 0) and (i % 5 == 0):
+            tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
         else:
-            print(i, end=" ")
-    # Ajouter une nouvelle ligne après l'affichage de la séquence
-    print()
-
-# Vérifie si le script est exécuté en tant que programme principal
-if __name__ == "__main__":
-    # Vérifie si un seul argument est passé (en plus du nom du script)
-    if len(sys.argv) != 2:
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
     try:
-        # Convertit l'argument en entier
         number = int(sys.argv[1])
-        # Appelle la fonction fizzbuzz avec le nombre en argument
         fizzbuzz(number)
     except ValueError:
-        # Affiche un message d'erreur si l'argument n'est pas un entier valide
-        print("Veuillez entrer un nombre valide.")
+        # Print an error message if the argument is not a valid integer
+        print("Please enter a valid number.")
+    
