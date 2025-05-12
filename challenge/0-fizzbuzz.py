4eae538e54c1fe8294d9916d776b322b39d0004f
TonNom <ton-email@example.com>
2025-05-12 10:26:07 +0200

    add problem bugg with fizzbuz files

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -5,11 +5,7 @@ import sys
 
 def fizzbuzz(n):
 
-    # Fonction qui affiche les nombres de 1 à n en respectant les règles de FizzBuzz :
-    # Si un nombre est divisible par 3 et 5, affiche "FizzBuzz"
-    # Si un nombre est divisible par 3, affiche "Fizz"
-    #Si un nombre est divisible par 5, affiche "Buzz"
-    #Sinon, affiche simplement le nombre
+
     for i in range(1, n + 1):
         # Vérifie d'abord si le nombre est divisible par 3 et 5 (priorité)
         if i % 3 == 0 and i % 5 == 0:
